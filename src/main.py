50d15e156051505265ed0d6ac7a4aef4f169d1ec
Kajal Gaikwad <96899081+KajalGaikwad22@users.noreply.github.com>
2023-12-15 22:46:36 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,28 +6,22 @@ from src.connectors.postgresql_connector import read_from_postgresql, write_to_p
 from src.formats.json_format import read_json, write_json
 
 def main():
-    # Create a Spark session
-    spark = SparkSession.builder.appName("YourAppName").getOrCreate()
 
-    # Read from PostgreSQL (assuming 'Taiyo' is the table name)
+    spark = SparkSession.builder.appName("TaiyoApp").getOrCreate()
+
     postgres_url = "jdbc:postgresql://:5432/PDdb"
     source_table = "Taiyo"
     df_postgres = read_from_postgresql(spark, postgres_url, source_table)
 
-    # Apply transformations
     df_grouped = group_by_measure(df_postgres, "name")
     
-    # Read from JSON (adjust file path accordingly)
     df_another = read_json(spark, "dataset.json")
 
-    # Perform join
     df_joined = join_datasets(df_grouped, df_another, "user_Id")
 
-    # Write to PostgreSQL (adjust 'your_target_table' accordingly)
     target_table = "Taiyo2"
     write_to_postgresql(df_joined, postgres_url, target_table)
 
-    # Write result to JSON (adjust file path accordingly)
     write_json(spark, df_joined, "output.json")
 
 if __name__ == "__main__":
